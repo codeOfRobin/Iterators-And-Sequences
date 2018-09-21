f5cdcca97a8d41babcd9ab958af8745cf33ab66c
Robin Malhotra <me@rmalhotra.com>
2018-09-21 18:50:56 +0530

    Added reserved keywords
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

---
+++ LazyPrefix/ReservedKeyboards.swift
@@ -0,0 +1,15 @@
+//
+//  ReservedKeyboards.swift
+//  LazyPrefix
+//
+//  Created by Robin Malhotra on 21/09/18.
+//  Copyright © 2018 Robin Malhotra. All rights reserved.
+//
+
+import Foundation
+
+enum `Type` {
+    case something
+}
+
+let x = `Type`.somethingt
